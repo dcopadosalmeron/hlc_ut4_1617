2229643b3c6be7c2b9e42c3ee9c0130cb81cc112
Luis Ramón López <lrlopez@gmail.com>
2017-03-05 23:48:49 +0100

    Incorporado formulario de entrada y la configuración de seguridad (apartado 6)

---
+++ src/AppBundle/Controller/UsuarioController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UsuarioController extends Controller
+{
+    /**
+     * @Route("/conectar", name="usuario_entrar")
+     */
+    public function loginAction()
+    {
+        $helper = $this->get('security.authentication_utils');
+
+        return $this->render(':Usuario:entrar.html.twig',
+            [
+                'last_username' => $helper->getLastUsername(),
+                'error'         => $helper->getLastAuthenticationError()
+            ]);
+
+    }
+
+    /**
+     * @Route("/comprobar", name="usuario_comprobar")
+     * @Route("/desconectar", name="usuario_salir")
+     */
+    public function operacionesLoginAction()
+    {
+
+    }
+}
