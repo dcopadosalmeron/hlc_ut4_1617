062403075ef909e24ca19ce27b53a0f48af3cd94
Luis Ramón López <lrlopez@gmail.com>
2017-03-05 20:29:54 +0100

    Añadidos ejemplos útiles de Bootstrap en la plantilla index

--- src/AppBundle/Controller/PartidoController.php
+++ src/AppBundle/Controller/PartidoController.php
@@ -2,17 +2,27 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="inicio")
+     * @Route("/", name="resultados")
      */
     public function indexAction()
     {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $em->createQueryBuilder()
+            ->select('p')
+            ->from('AppBundle:Partido', 'p')
+            ->orderBy('p.fechaCelebracion', 'DESC')
+            ->getQuery()
+            ->getResult();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig');
     }
